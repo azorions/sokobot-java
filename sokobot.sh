1c425c61a3719a33a76b623404fa77b5ce9f53b4
azorions <ivanryes05@gmail.com>
2026-06-06 16:03:38 +0800

    refactor: delete .class files and add State.java

--- sokobot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find . -name "*.class" -type f -delete
-javac src/main/Driver.java -cp src
-java -classpath src main.Driver "$1" bot
---
+++ src/solver/State.java
@@ -0,0 +1,12 @@
+package solver;
+
+public class State {
+    
+
+    // Constructor
+    public State() {
+        
+        
+        // Initialize state variables here
+    }
+}
